ec232ba0bd6e6cdfc0df38fce6206148211a89eb
LucasFontini1 <lucasfontinidaluz@gmail.com>
2025-10-14 18:40:48 -0300

    Primeiro commit Tela00s, só criei o projeto vazio!

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import path from 'node:path'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: home,
+    }
+  ],
+})
+
+export default router
