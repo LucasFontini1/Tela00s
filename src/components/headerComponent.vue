108a2d0b8ef2154b1c3de5b4de8c9ac333647a5c
LucasFontini1 <lucasfontinidaluz@gmail.com>
2025-10-19 12:51:10 -0300

    FEAT: Fiz mudanças no header, e fiz a parte das series mais bem avaliadas #5

--- src/components/headerComponent.vue
+++ src/components/headerComponent.vue
@@ -1,17 +1,27 @@
 <script setup>
-    import { RouterLink } from 'vue-router';
+import { RouterLink, useRoute } from 'vue-router'
+import { computed } from 'vue'
+
+const route = useRoute()
+
+const currentPath = computed(() => route.path)
 </script>
 <template>
     <header>
         <nav>
-            <RouterLink to="/"><img src=" /logo.png" alt=""></RouterLink>
-            <ul>
+            <h1>
+                <RouterLink to="/"><img src="/public/logo.png" alt="LOGO"></RouterLink>
+            </h1>
+            <ul class="pages">
                 <li>
-                    <RouterLink to="/">Home</RouterLink>
+                    <RouterLink to="/" :class="{ paginaAtual: currentPath === '/' }">INICIO</RouterLink>
                 </li>
                 <li>
-                    <RouterLink to="/series">Séries</RouterLink>
+                    <RouterLink to="/series" :class="{ paginaAtual: currentPath === '/series' }">SERIES</RouterLink>
                 </li>
+
+            </ul>
+            <ul class="icons">
                 <li>
                     <span class="mdi mdi-magnify"></span>
                 </li>
@@ -20,31 +30,55 @@
     </header>
 </template>
 <style scoped>
-    nav {
-        display: flex;
-        justify-content: space-between;
-        padding: 1rem 2rem;
-        align-items: center;
-    }
-    img{
-        width: 7rem;
-        height: auto;
-    }
-    ul {
-        list-style: none;
-        display: flex;
-        gap: 2rem;
-        margin-right: 2rem;
-        align-items: center;
-    }
-    li a{
-        font-size: 1.5rem;
-        color: black;
-        text-decoration: none;
-        font-family: "Quantico", sans-serif;
-    }
-    span{
-        cursor: pointer;
-        font-size: 1.8rem;
-    }
+nav {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1rem 2rem;
+}
+
+nav h1 img {
+    width: 100%;
+    cursor: pointer;
+}
+
+nav ul {
+    display: flex;
+    gap: 2rem;
+    list-style: none;
+    justify-content: left;
+}
+
+nav ul li {
+    cursor: pointer;
+    font-weight: bold;
+}
+
+nav ul.pages {
+    background-color: #1B5EB8;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: 0.5rem 1rem;
+    border-radius: 8px;
+    border: #0F3363 5px solid;
+}
+
+nav ul.pages li a {
+    color: white;
+    text-decoration: none;
+    font-family: 'Krona One', sans-serif;
+    padding: 0.3rem 1rem;
+}
+
+header nav ul.pages li a.paginaAtual {
+    background-color: #0F3363;   
+    border-radius: 4px;
+}
+
+ul.icons li span {
+    font-size: 2rem;
+    color: #1B5EB8;
+}
+
 </style>
