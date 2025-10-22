ba63e019661187c6b98b87919edf8102cb6a3908
“LucasFontini1” <lucasfontinidaluz@gmail.com>
2025-10-22 15:12:19 -0300

    FEAT: Falta adicionar o link do git dos devs #16

--- src/views/homeView.vue
+++ src/views/homeView.vue
@@ -2,11 +2,13 @@
     import headerComponent from '../components/headerComponent.vue';
     import searchComponent from '@/components/searchComponent.vue';
     import bestSeriesComponent from '@/components/bestSeriesComponent.vue';
+    import footerComponent from '@/components/footerComponent.vue';
 </script>
 <template>
     <headerComponent />
     <searchComponent />
     <bestSeriesComponent />
+    <footerComponent />
 </template>
 <style scoped>
 
