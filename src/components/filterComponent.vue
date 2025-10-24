7f2a3940cb6f33fc1a9a034a4273591b676a7d5a
“LucasFontini1” <lucasfontinidaluz@gmail.com>
2025-10-24 09:31:52 -0300

    FEAT: iniciei o filter component #22

---
+++ src/components/filterComponent.vue
@@ -0,0 +1,38 @@
+<script setup>
+    import { ref } from 'vue';
+
+
+    const isOpen = ref(false);
+    function toggleFilter() {
+        isOpen.value = !isOpen.value;
+    }
+</script>
+<template>
+    <section class="filter">
+        <div class="alwais-open">
+            <span @click="toggleFilter()" class="mdi mdi-menu menu" v-if="!isOpen"></span>
+            <span @click="toggleFilter()" class="mdi mdi-menu-close menu" v-if="isOpen"></span>
+            <h2>Filtrar</h2>
+        </div>
+    </section>
+</template>
+<style scoped>
+    .filter {
+        padding: 20px;
+        background-color: #1B5EB8;
+        width: 30%;
+        margin: 0 auto;
+        text-align: center;
+        color: white
+    }
+    .alwais-open {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+        cursor: pointer;
+        justify-content: center;
+    }
+    .menu {
+        font-size: 2rem;
+    }
+</style>
